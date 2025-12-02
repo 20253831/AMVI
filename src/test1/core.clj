42dc309272f8339cc651a6c9ad126ced9924539e
Luka Vukmirovic <lv20253831@student.fon.bg.ac.rs>
2025-12-03 00:57:23 +0100

    03.12.2025.

--- src/test1/core.clj
+++ src/test1/core.clj
@@ -1,6 +1,28 @@
 (ns test1.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;zaokruzivanje na 2 decimale
+(defn round2 [x]
+  (/ (Math/round (* x 100.0)) 100.0))
+
+;"Izracunavanje OIE (obnovljivi izvori energije)."
+(defn OIE [x]
+  (round2 (* x 0.093)))
+
+;izracunavanje cene potrosene ee
+(defn EE [x]
+  (if (> x 1600)
+    (+ (* (- x 1600) 15.33) (* 1250 7.666) (* 350 5.11))
+    (if (> x 350)
+      (+ (* (- x 350) 7.666) (* 350 5.11))
+      (* x 5.11))))
+
+;naknada za trosak javnog snabdevaca fiksna u zavisnosti od mesta isporuke
+
+;obracunska snaga - malo komplikovanije, zavisi od mesecne odobrene snage i placa se po kW
+
+;akciza 7.5%
+
+;pdv 20%
+
+;JMS 150 RSD (javni medijski servis)
+
